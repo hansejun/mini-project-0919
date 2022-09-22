c11074777a82d853dcdbfe06468229f8cee5e47f
hansejun <tpwnszja2@gmail.com>
2022-09-22 09:57:04 +0900

    dateConversion Fn

--- static/js/util/addHtml.js
+++ static/js/util/addHtml.js
@@ -31,7 +31,7 @@ export const addHtml = (cafe, reviews, userId) => {
       <span class="review-name">${review.userId}</span>
     </div>
     <div class="container-item-review">
-      <span class="review-createdAt">${review.createdAt}</span>
+      <span class="review-createdAt">${dateConversion(review.createdAt)}</span>
       <pre class="review-comment">${review.comment}</pre>
       <div class="review-fn">
         <span class="review-rate">
@@ -64,4 +64,19 @@ export const addHtml = (cafe, reviews, userId) => {
   deleteBtns.forEach((deleteBtn) => {
     deleteBtn.addEventListener("click", handleDelete);
   });
+};
+
+const dateConversion = (date) => {
+  const nowDateString = new Date().toISOString().substring(2, 10);
+  const nowDate = nowDateString.split("-").map((item) => Number(item));
+  const compareDate = date.split("-").map((item) => Number(item));
+  if (nowDate[0] > compareDate[0]) {
+    return `${nowDate[0] - compareDate[0]}년전`;
+  } else if (nowDate[1] > compareDate[1]) {
+    return `${nowDate[1] - compareDate[1]}달전`;
+  } else if (nowDate[2] > compareDate[2]) {
+    return `${nowDate[2] - compareDate[2]}일전`;
+  } else {
+    return "오늘";
+  }
 };
